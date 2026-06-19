5b8a40fafff21c1d2c7eebd341db64e8cc07e3a0
ni-cookie <lapin.n14@gmail.com>
2026-06-19 08:53:55 +0300

    fix: add frontend source code properly

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { navigating } from '$app/state';
+	import favicon from '$lib/assets/favicon.ico';
+	import Nav from './Nav.svelte';
+	import PreloadingIndicator from './PreloadingIndicator.svelte';
+
+	const { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{#if navigating.from}
+	<PreloadingIndicator />
+{/if}
+
+<Nav />
+
+<main>
+	{@render children()}
+</main>
